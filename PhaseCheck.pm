c94f91642f2672434c7233bc5e28675fe29df7b1
binary-maksym <maksym@binary.com>
2015-12-22 17:59:20 +0800

    test

--- PhaseCheck.pm
+++ PhaseCheck.pm
@@ -1,7 +1,42 @@
+package Feed::PhaseCheck;
+
+use 5.006;
 use strict;
 use warnings;
 
-package PhaseCheck;
+=head1 NAME
+
+Feed::PhaseCheck - The great new Feed::PhaseCheck!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Feed::PhaseCheck;
+
+    my $foo = Feed::PhaseCheck->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
 
 sub new {
     my ($class, $args) = @_;
@@ -15,58 +50,52 @@ sub set {
     if (ref $params eq 'HASH') {
         foreach (keys %$params) {
             if ($_ eq 'spots') {
-                $self->setSpots($params->{$_});
+                $self->set_spots($params->{$_});
                 delete $params->{$_};
             } elsif ($_ eq 'ref') {
-                $self->setRef($params->{$_});
+                $self->set_ref($params->{$_});
                 delete $params->{$_};
             } elsif ($_ eq 'short') {
-                $self->setShort($params->{$_});
+                $self->set_short($params->{$_});
                 delete $params->{$_};
             }
         }
     }
 }
 
-sub setSpots {
+sub set_spots {
     my ($self, $spots) = @_;
     if (ref $spots ne 'HASH') {
         return;
     }
     $self->{spots} = $spots;
-    delete $self->{errors};
-    delete $self->{min_errors};
-    delete $self->{ticks_number};
-    return 1;
+    $self->clean;
+    return $self->{spots};
 }
 
-sub setRef {
+sub set_ref {
     my ($self, $ref) = @_;
     if ($ref) {
         $self->{ref} = $ref;
-        delete $self->{errors};
-        delete $self->{min_errors};
-        delete $self->{ticks_number};
-        return 1;
+        $self->clean;
     }
+    return $self->{ref};
 }
 
-sub setShort {
+sub set_short {
     my ($self, $short) = @_;
     if ($short) {
         $self->{short} = $short;
-        delete $self->{errors};
-        delete $self->{min_errors};
-        delete $self->{ticks_number};
-        return 1;
+        $self->clean;
     }
+    return $self->{short};
 }
 
 sub list_errors {
     my ($self, $provider) = @_;
 
     if ($self->{errors}) {
-        $self->calculate_errors;
+        ($self->{errors}, $self->{min_errors}, $self->{last_ref_epoch}) = $self->calculate_errors;
     }
 
     if ($provider) {
@@ -80,7 +109,7 @@ sub list_min_errors {
     my ($self, $provider) = @_;
 
     if ($self->{errors}) {
-        $self->calculate_errors;
+        ($self->{errors}, $self->{min_errors}, $self->{last_ref_epoch}) = $self->calculate_errors;
     }
 
     if ($provider) {
@@ -109,9 +138,7 @@ sub calculate_errors {
         die();
     }
 
-    $self->{errors}       = {};
-    $self->{min_errors}   = {};
-    $self->{ticks_number} = {};
+    $self->clean;
 
     my $ref = {
         epoches => [sort(keys %{$self->{spots}->{$self->{ref}}})],
@@ -119,9 +146,10 @@ sub calculate_errors {
 
     my $first_ref_epoch = $ref->{epoches}->[0];
     my $last_ref_epoch  = $ref->{epoches}->[-1];
-    $self->{last_ref_epoch} = $last_ref_epoch;
 
-    my $delay = int(($ref->{epoches}->[-1] - $ref->{epoches}->[0] - $self->{short}) / 2);
+    my $delay      = int(($ref->{epoches}->[-1] - $ref->{epoches}->[0] - $self->{short}) / 2);
+    my $errors     = {};
+    my $min_errors = {};
     foreach my $provider (keys %{$self->{spots}}) {
         next if $provider eq $self->{ref};
         my %spots;
@@ -136,9 +164,11 @@ sub calculate_errors {
             spots   => \%spots
         };
         if (scalar keys %spots) {
-            ($self->{errors}->{$provider}, $self->{min_errors}->{$provider}) = $self->calculate_error($ref, $sample);
+            ($errors->{$provider}, $min_errors->{$provider}) = $self->calculate_error($ref, $sample);
         }
+
     }
+    return ($errors, $min_errors, $last_ref_epoch);
 }
 
 sub calculate_error {
@@ -181,4 +211,100 @@ sub calculate_error {
         });
 }
 
-1;
+sub clean{
+    my $self = shift;
+    $self->{errors}       = {};
+    $self->{min_errors}   = {};
+    $self->{ticks_number} = {};
+}
+
+=head1 AUTHOR
+
+Maksym Kotielnikov, C<< <maksym at binary.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-feed-phasecheck at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Feed-PhaseCheck>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Feed::PhaseCheck
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Feed-PhaseCheck>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Feed-PhaseCheck>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Feed-PhaseCheck>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Feed-PhaseCheck/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Maksym Kotielnikov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+1;    # End of Feed::PhaseCheck
